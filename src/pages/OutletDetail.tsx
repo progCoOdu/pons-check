4405aa8e3c51cc2dc964b50cf1e54526a9262932
progCoOdu <work.chill.coffee@gmail.com>
2026-09-13 11:20:38 +0300

    Update OutletDetail.tsx

--- src/pages/OutletDetail.tsx
+++ src/pages/OutletDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react' 
 import { supabase } from '../supabase'
 import { Outlet, Inspector, Inspection } from '../types'
 import InspectionFlow from './InspectionFlow'
